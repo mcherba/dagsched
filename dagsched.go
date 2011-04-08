fa4ec9a10bc1dbf9822957eec7f413f90d3564f3
Mike Cherba <mcherba@gmail.com>
2011-04-08 00:33:45 -0700

    Updated makefile to use parse.go and sorter.go on both 32 and 64 bit systems

--- dagsched.go
+++ dagsched.go
@@ -6,13 +6,16 @@ package main
 
 import fmt "fmt" // Package implementing formatted I/O.
 import flag "flag" // Command line parsing
-import dag "./dag" // structure and code for DAGs
+import parser "./parser" // structure and code for DAGs
+import sorter "./sorter" // topological sort
+//import vector "container/vector"
 func main() { 
 	
 	// Command line flags	
 	var numcores *int = flag.Int("n", 2, "number of cores to use in the simulation [-n Int value]")
 	var infname *string = flag.String("f", "infile.dag", "filename to load the .dag from [-f filename.dag]")
 	var algtype *string = flag.String("a", "t-level", "algorithm type to use t-level, b-level, or ??")
+	
 
 	flag.Parse()
 
@@ -20,8 +23,14 @@ func main() {
 	fmt.Printf("loading DAG from %s\n", *infname)
 	fmt.Printf("using %s scheduling algorithm\n", *algtype)
 	
-	//var thisdag dag.Dag
-	dag.Itest()
-	dag.PrintDag()
+	
+	// Read in the dag we want to schedule
+	var dag = parser.ParseFile(*infname)
+	//parser.PrintDAG(dag)
+	
+	// topo sort it
+	var tsdag = sorter.TopSort(dag, 't')
+	//fmt.Printf("%v", tsdag)
+	
 	
 } 
